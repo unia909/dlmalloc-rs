b7abadb1aee177b5ead24220513ea6a1b971ddb3
Danil Smirnov <yrakrulasov@gmail.com>
2023-11-12 17:10:21 +0300

    dummy impl

--- src/dummy.rs
+++ src/dummy.rs
@@ -1,19 +1,26 @@
 use core::ptr;
 use Allocator;
 
-pub struct System {
-    _priv: (),
-}
+pub struct System;
 
 impl System {
     pub const fn new() -> System {
-        System { _priv: () }
+        System
     }
 }
 
 unsafe impl Allocator for System {
-    fn alloc(&self, _size: usize) -> (*mut u8, usize, u32) {
-        (ptr::null_mut(), 0, 0)
+    fn alloc(&self, mut size: usize) -> (*mut u8, usize, u32) {
+        let real_size = size;
+        core::arch::asm!(
+            "li x0, 7",
+            inout("a0") size,
+            options(pure, nomem, nostack)
+        );
+        if size == u32::MAX as usize {
+            return (ptr::null_mut(), 0, 0);
+        }
+        (size as *mut u8, real_size, 0)
     }
 
     fn remap(&self, _ptr: *mut u8, _oldsize: usize, _newsize: usize, _can_move: bool) -> *mut u8 {
@@ -33,10 +40,10 @@ unsafe impl Allocator for System {
     }
 
     fn allocates_zeros(&self) -> bool {
-        false
+        true
     }
 
     fn page_size(&self) -> usize {
-        1
+        64 * 1024
     }
 }
